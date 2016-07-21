eb3976c60afa6fafdc3d1ac0447575a8b8f60a72
Ahkeem Lang <langahkeem@gmail.com>
2016-07-21 18:35:12 -0500

    Built json into my users_controller.

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,2 +1,19 @@
 class PokemonsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,45 @@
 class UsersController < ApplicationController
+  def index
+    render json: User.all, status: 200
+  end
+
+  def show
+    if user.exists?(params{user.id})
+      render User.all[user_id].to_json, status: 200
+    else
+      render json: { message: "Not Found" }, status: 400
+    end
+  end
+
+  def edit
+  end
+
+  def create
+    User.new(id: params[:id])
+
+    if user.save
+      render json: user.to_json, status: 200
+    else
+      render json: user.errors.to_json, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    user.find (params[:id])
+    if user.update(user_id: params[:user_id], quantity: params[:quantity])
+      render json: order.to_json, status: 200
+    else
+      render json: order.errors.to_json, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if User.exists?(params[:id])
+      User.destroy(params[:id])
+      render json: { message: "User destroyed." }, status: 200
+    else
+      render json: { message: "User not found." }, status: 400
+    end
+
+  end
 end
