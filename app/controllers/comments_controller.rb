a0776e579169e44d86ce54dfef404aa36c585787
Ahkeem Lang <langahkeem@gmail.com>
2016-07-22 10:43:52 -0500

    CRUD completed for comment controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,37 @@
+class CommentsController < ApplicationController
+  def index
+    render json: comment.all, status: 200
+  end
+
+  def show
+    if Comment.exists?(params{comment_id})
+      render Comment.all[comment.id].to_json, status: 200
+    else
+      render json: { message: "Not Found" }, status: 400
+    end
+  end
+
+  def edit
+  end
+
+  def create
+    Comment.new(id: params[:id])
+
+    if comment.save
+      render json: comment.to_json, status: 200
+    else
+      render json: comment.errors.to_json, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    pokemon.find (params[:id])
+    if comment.update(pokemon_id: params[:pokemon_id], quantity: params[:quantity])
+    else
+      render json: comment.errors.to_json, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+  end
+end
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,12 +3,24 @@ class LikesController < ApplicationController
   end
 
   def show
+    if like.exists?(params{like.id})
+      render Like.all[like_id].to_json, status: 200
+    else
+      render json: { message: "Not Found" }, status: 400
+    end
   end
 
   def edit
   end
 
   def create
+    Like.new(id: params[:id])
+
+    if user.save
+      render json: like.to_json, status: 200
+    else
+      render json: like.errors.to_json, status: :unprocessable_entity
+    end
   end
 
   def update
