ed5dd2c30032482cd836c9f702cb23c66a3abf51
Ahkeem Lang <langahkeem@gmail.com>
2016-07-21 21:24:13 -0500

    Finished another controller

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,17 +1,35 @@
 class PokemonsController < ApplicationController
   def index
+    render json: pokemon.all, status: 200
   end
 
   def show
+    if pokemon.exists?(params{pokemon_id})
+      render Pokemon.all[pokemon.id].to_json, status: 200
+    else
+      render json: { message: "Not Found" }, status: 400
+    end
   end
 
   def edit
   end
 
   def create
+    Pokemon.new(id: params[:id])
+
+    if pokemon.save
+      render json: pokemon.to_json, status: 200
+    else
+      render json: pokemon.errors.to_json, status: :unprocessable_entity
+    end
   end
 
   def update
+    pokemon.find (params[:id])
+    if pokemon.update(pokemon_id: params[:pokemon_id], quantity: params[:quantity])
+    else
+      render json: pokemon.errors.to_json, status: :unprocessable_entity
+    end
   end
 
   def destroy
