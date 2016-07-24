d4f877ad98dc520ff309427ded2e67eda05beb09
Ahkeem Lang <langahkeem@gmail.com>
2016-07-24 15:58:13 -0500

    Working on my edit method for my users controller still

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  # before_action :set_user, only: [:show, :edit, :update, :destroy]
-  # before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit]
   def index
     render json: User.all, status: 200
   end
