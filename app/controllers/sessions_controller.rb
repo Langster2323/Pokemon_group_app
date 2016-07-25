0bee9c9d2e73df94f069d83e05ce3ba1688750f2
Ahkeem Lang <langahkeem@gmail.com>
2016-07-24 21:06:41 -0500

    added user_id: user.id

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,7 +18,7 @@ class SessionsController < ApplicationController
     # end
     user = User.find_by(username: params[:username])
     if user.present? && user.authenticate(params.fetch(:password))
-      render json: {authtoken: user.authtoken}, status: 200
+      render json: {authtoken: user.authtoken, user_id: user.id}, status: 200
     # render json: {authtoken: user.authtoken}
     else
       render json: { message: "Not found"}, status: 401
