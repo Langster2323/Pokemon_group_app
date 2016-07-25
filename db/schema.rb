994638ca5a2db08769436dcfa5bffac2a1304f21
Ahkeem Lang <langahkeem@gmail.com>
2016-07-24 23:28:37 -0500

    Had to delete favorite from users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160725022645) do
+ActiveRecord::Schema.define(version: 20160725042530) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,9 +43,9 @@ ActiveRecord::Schema.define(version: 20160725022645) do
     t.string   "username",        null: false
     t.string   "password_digest", null: false
     t.string   "authtoken",       null: false
+    t.string   "favorite",        null: false
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.string   "favorite"
     t.string   "profileImg"
     t.string   "description"
   end
